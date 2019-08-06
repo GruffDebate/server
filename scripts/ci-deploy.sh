d9438e064c60ecc4f03509f5edb27de050d07d8c
Bruno Sato <bruno.sato@live.com>
2019-08-06 20:48:55 -0300

    fix ci

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -20,4 +20,4 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
+  apply -f ./kube/server-deployment.yml
